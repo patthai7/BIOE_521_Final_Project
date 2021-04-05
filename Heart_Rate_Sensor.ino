5be3804cb49fc38e884b35149a9fd87d4b27a4c8
patthai7 <pthai1225@gmail.com>
2021-04-05 15:10:30 -0500

    Clean up code and add comments

--- Heart_Rate_Sensor.ino
+++ Heart_Rate_Sensor.ino
@@ -1,20 +1,31 @@
 //LIBRARIES--------------------------------------------------
+
+// include the Adafruit Circuit Playground library
 #include <Adafruit_CircuitPlayground.h>
 
-#define window_size 5
+// define how many readings will be used in the moving average
+// filter
+#define num_readings 5
 
 //VARIABLES--------------------------------------------------
 
 // set up a variable for the light sensor measurement
 int light_sensor_value;
 
+// initialize the index, value, and sum for the moving 
+// average filter
 int index = 0;
 int value = 0;
 int sum = 0;
-int readings[window_size];
-int averaged = 0;
 
-int threshold = 50;
+// initialize an array that holds the reading values and
+// is the size of the number of readings for the moving 
+// average filter
+int readings[num_readings];
+
+// initialize the averaged value for the specified number 
+// of measurements for the moving average filter
+int averaged = 0;
 
 //SETUP------------------------------------------------------
 void setup() {
@@ -42,22 +53,44 @@ void setup() {
 //LOOP-------------------------------------------------------
 void loop() {
 
+  // read the value from the light sensor on the Adafruit
+  // Circuit Playground
   light_sensor_value = CircuitPlayground.lightSensor();
+
+  // print the light sensor value on the serial monitor
+  // and have it display as a variable on the serial plotter
   Serial.print("LightSensorValue:");
   Serial.print(light_sensor_value);
   Serial.print(",");
 
+  // Moving Average Filter
+
+  // for each index, remove the oldest reading value to make 
+  // room for a new reading value
   sum = sum - readings[index];
+
+  // set the readings vector at the specified index to the
+  // light sensor value
   readings[index] = light_sensor_value;
+
+  // add the newest light sensor value to the sum of the
+  // readings
   sum = sum + light_sensor_value;
-  index = (index + 1)%window_size;
 
-  averaged = sum/window_size;
+  // go through the specified index values until it reaches 
+  // the maximum index value, then go back to the first index
+  // value of 0
+  index = (index + 1)%num_readings;
 
-//  Serial.print("LightSensorValueAverage: ");
+  // take the average of the recorded readings
+  averaged = sum/num_readings;
+
+  // print the averaged light sensor value on the serial monitor
+  // and have it display as a variable on the serial plotter
   Serial.print("LightSensorAverageValue:");
   Serial.println(averaged);
 
+  // delay the loop for 50 milliseconds
   delay(50);
   
 }
